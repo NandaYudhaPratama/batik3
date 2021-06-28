51a79a1a9bfdbb0db253fcbdb19682f92d5cf604
Nanda yudha pratama <nanda.yudha.ft18@mail.umy.ac.id>
2021-06-28 21:49:47 +0700

    home page kosong

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,27 +3,27 @@
 @section('content')
  <div class="container">
  <div class="row" style="margin-top:15px;">
- <!-- @foreach ($blogs as $blog) -->
+
     
       <div class="col-md-4">
         <div class="card h-100 shadow p-3 bg-white rounded">
           <img src="..." class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">
-            <!-- {{$produk->nama}} -->
+
             </h5>
             <p class="card-text">
-            <!-- {{$produk->size}} -->
+
             </p>
             <div class="card-footer">
             <small class="text-muted">
-            <!-- {{$produk->harga}} -->
+
             </small>
             </div>
           </div>
         </div>
       </div>
-       <!-- @endforeach -->
+
     </div>
  
 </div>
