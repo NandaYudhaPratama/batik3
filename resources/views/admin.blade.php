645855fa8221311ffeb676b0a8781561a1ea4ecf
Nanda yudha pratama <nanda.yudha.ft18@mail.umy.ac.id>
2021-07-07 00:07:56 +0700

    login fix

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,46 +1,29 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Toko Batik</title>
-  </head>
-  <body>
 
-    <div class="container">
-        <div class="row" style="margin-top:160px">
-            <div class="col-sm">
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Login Form with floating placeholder and light button</title>
+  <link rel="stylesheet" href="{{ asset('/LoginAdmin/style.css')}}">
 
-            </div>
-            <div class="col-sm card h-100 shadow p-3 bg-white rounded">
-                <form name="loginForm" method="get" action="{{url('/blog')}}">
-                    <div style="margin-left:auto;margin-right:auto; width:25%;padding-bottom:20px; padding-top:20px;">
-                        <img src="{{ asset('img/profil.png')}}" height="90px" width="90px">
-
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input style=" border-radius: 50px;" name="Email" class="form-control" id="floatingInput" placeholder="name@example.com" style="background: transparent;">
-                        <label for="floatingInput">Email</label>
-                    </div>
-                    <div class="form-floating">
-                        <input style="border-radius: 50px;" name="Password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                        <label for="floatingPassword">Password</label>
-                    </div>
-                    <br />
-                    <input type="submit" class="btn btn-primary" onclick="return check(this.form)" value="Login">
-                </form>
-            </div>
-            <div class="col-sm">
-
-            </div>
-
-        </div>
-        <script language="javascript">
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="login-box">
+  <h2>Login</h2>
+  <form name="loginForm" method="get" action="{{url('/blog')}}">
+    <div class="user-box">
+      <input type="text" name="Email" required="">
+    </div>
+    <div class="user-box">
+      <input type="password" name="Password" required="">
+    </div> 
+    <input type="submit" class="btn btn-primary" onclick="return check(this.form)" value="Login">
+</input>
+  </form>
+  <script language="javascript">
             function check(form) {
 
                 if (form.Email.value == "admin@gmail.com" && form.Password.value == "123") {
@@ -52,18 +35,9 @@
                 }
             }
         </script>
-    </div>
+</div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
+<!-- partial -->
+  
+</body>
 </html>
-
